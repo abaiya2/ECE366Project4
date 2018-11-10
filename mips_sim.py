4c9d82b17ffc290fb146a9a902e0626a98d503cd
Amaan Baiyat <abaiya2@uic.edu>
2018-11-10 00:51:04 -0600

    Added i_mem.txt file and simulator function

--- mips_sim.py
+++ mips_sim.py
@@ -1,1 +1,17 @@
 print("ECE 366 Project 4: MIPS Simulator")
+
+
+def file_to_array(file):
+    return_array = []
+    for line in file:
+        return_array.append(line.rstrip())
+    return return_array
+
+
+def simulator(instr_mem_file_name):
+    instr_mem_file = open(instr_mem_file_name, "r")
+    instr_mem = file_to_array(instr_mem_file)
+    print(instr_mem)
+
+
+simulator("i_mem.txt")
